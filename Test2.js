2cd21e0f26c87b201a57a2b8c297777efd53ca5c
Alex <aleck.boldareff2014@gmail.com>
2021-08-03 13:01:12 +0300

    added console.log

--- Test2.js
+++ Test2.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 class Test2 extends React.Component {
     render() {
+        console.log('То что нужно удалить')
         return (
             <h2>Test 2 Component</h2>
+            
         );
     }
 }
