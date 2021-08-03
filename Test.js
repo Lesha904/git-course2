6aad734f7f5023c7827b9dfe93d0cbbfc6ebb8f9
Alex <aleck.boldareff2014@gmail.com>
2021-08-03 14:10:01 +0300

    change log on добрый вечер

--- Test.js
+++ Test.js
@@ -2,6 +2,7 @@ import Test2 from './Test2';
 
 function Test() {
     console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    console.log('добрый вечер');
     return (
         <>
             <h1>Test component 1</h1>
